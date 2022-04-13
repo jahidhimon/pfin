38496d82871d84527175413cbdbba9b57bdc6f1f
jahidhimon <jahidhasanimonn@gmail.com>
2022-04-13 14:21:59 +0600

    healthcheck endpoint

--- internal/data/transaction.go
+++ internal/data/transaction.go
@@ -2,11 +2,20 @@ package data
 
 import "time"
 
-type Transaction struct {
-	Amount    float32   `json:"amount"`
-	Date      time.Time `json:"time"`
-	Necessity int8      `json:"necessity"`
-	Category  string    `json:"category"`
-	Reason    string    `json:"reason"`
-}
+type TransactionType uint8
 
+const (
+	Income TransactionType = iota
+	Expense
+	ExpectedIncome
+)
+
+type Transaction struct {
+	Amount       float32         `json:"amount"`
+	Date         time.Time       `json:"time"`
+	Necessity    int8            `json:"necessity"`
+	Type         TransactionType `json:"transaction_type"`
+	MainCategory []string        `json:"main_category"`
+	SubCategory  string          `json:"sub_category"`
+	Reason       string          `json:"reason"`
+}
